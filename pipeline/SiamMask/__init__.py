7991a09acbdd520d141221381dd3902e8c874318
Lionel Miller <delonorm@gmail.com>
2019-07-14 22:19:53 +0300

    Reformat Siamese __init__

--- pipeline/SiamMask/__init__.py
+++ pipeline/SiamMask/__init__.py
@@ -1,17 +1,19 @@
-import os,sys
-sys.path = [os.path.dirname(__file__),os.path.join(os.path.dirname(__file__),'experiments/siammask_sharp')]+sys.path
-import glob
+import os
+import sys
+
+sys.path = [os.path.dirname(__file__), os.path.join(os.path.dirname(__file__), 'experiments/siammask_sharp')] + sys.path
 from tools.test import *
 from easydict import EasyDict
 from custom import Custom
 import numpy as np
 
+
 class SiamTracker(object):
-    def __init__(self,resume=os.path.join(os.path.dirname(__file__),'experiments/siammask_sharp/SiamMask_DAVIS.pth'),\
-                 config=os.path.join(os.path.dirname(__file__),'experiments/siammask_sharp/config_davis.json')):
+    def __init__(self, resume=os.path.join(os.path.dirname(__file__), 'experiments/siammask_sharp/SiamMask_DAVIS.pth'), \
+                 config=os.path.join(os.path.dirname(__file__), 'experiments/siammask_sharp/config_davis.json')):
         self.args = EasyDict(resume=resume,
                              config=config)
-        
+
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         torch.backends.cudnn.benchmark = True
         # Setup Model
@@ -20,10 +22,10 @@ class SiamTracker(object):
         assert isfile(self.args.resume), 'Please download {} first.'.format(self.args.resume)
         self.siammask = load_pretrain(self.siammask, self.args.resume)
         self.siammask.eval().to(self.device)
-        
-    def get_state(self,im,bbox):
-        x,y = bbox[0],bbox[1]
-        w,h = bbox[2]-x,bbox[3]-y
+
+    def get_state(self, im, bbox):
+        x, y = bbox[0], bbox[1]
+        w, h = bbox[2] - x, bbox[3] - y
         target_pos = np.array([x + w / 2, y + h / 2])
         target_sz = np.array([w, h])
         state = siamese_init(im, target_pos, target_sz, self.siammask, self.cfg['hp'], device=self.device)
@@ -31,44 +33,40 @@ class SiamTracker(object):
         state['box'] = bbox
         state['score'] = 1.
         return state
-    
-    def track(self,state,im):
-        new_state = siamese_track(state, im,self.siammask, mask_enable=False, refine_enable=False, device=self.device)
-        new_state['track_id'] = state['track_id']        
+
+    def track(self, state, im):
+        new_state = siamese_track(state, im, self.siammask, mask_enable=False, refine_enable=False, device=self.device)
+        new_state['track_id'] = state['track_id']
         p = new_state['target_pos']
         sz = new_state['target_sz']
-        new_state['box'] = np.concatenate((p-sz/2,p+sz/2))
+        new_state['box'] = np.concatenate((p - sz / 2, p + sz / 2))
         return new_state
-    
-
-def IoU(a,b):
-    aa = (a[:,2:]-a[:,:2])
-    aa = aa[:,0]*aa[:,1]
-    ab = (b[:,2:]-b[:,:2])
-    ab = ab[:,0]*ab[:,1]
-    
-    a = a[:,None,:]+np.zeros_like(b)[None,:,:]
-    b = b[None,:,:]+np.zeros_like(a)
-    i = np.clip((np.minimum(a[:,:,2:],b[:,:,2:]) - np.maximum(a[:,:,:2],b[:,:,:2])),0,1e10)
-    i = i[:,:,0]*i[:,:,1]
-    return np.clip(i/(aa[:,None]+ab[None,:]-i),0,1)
 
 
+def IoU(a, b):
+    aa = (a[:, 2:] - a[:, :2])
+    aa = aa[:, 0] * aa[:, 1]
+    ab = (b[:, 2:] - b[:, :2])
+    ab = ab[:, 0] * ab[:, 1]
 
+    a = a[:, None, :] + np.zeros_like(b)[None, :, :]
+    b = b[None, :, :] + np.zeros_like(a)
+    i = np.clip((np.minimum(a[:, :, 2:], b[:, :, 2:]) - np.maximum(a[:, :, :2], b[:, :, :2])), 0, 1e10)
+    i = i[:, :, 0] * i[:, :, 1]
+    return np.clip(i / (aa[:, None] + ab[None, :] - i), 0, 1)
 
 
 class SiamMultiTracker(object):
-    def __init__(self,min_iou=0.5,min_det_conf=0.5,min_track_score=0.9,\
-                 resume=os.path.join(os.path.dirname(__file__),'experiments/siammask_sharp/SiamMask_DAVIS.pth'),\
-                 config=os.path.join(os.path.dirname(__file__),'experiments/siammask_sharp/config_davis.json')):
-        self.tracker = SiamTracker(resume=resume,config=config)
+    def __init__(self, min_iou=0.5, min_det_conf=0.5, min_track_score=0.9, \
+                 resume=os.path.join(os.path.dirname(__file__), 'experiments/siammask_sharp/SiamMask_DAVIS.pth'), \
+                 config=os.path.join(os.path.dirname(__file__), 'experiments/siammask_sharp/config_davis.json')):
+        self.tracker = SiamTracker(resume=resume, config=config)
         self.states = []
         self.min_iou = min_iou
         self.min_det_conf = min_det_conf
         self.min_track_score = min_track_score
-        
 
-    def update(self,img,bboxes):
+    def update(self, img, bboxes):
         '''
         yield imname,img,bboxes(x1,y1,x2,y2,score,class,sclass_score,track_id,track_score)
         '''
@@ -102,7 +100,8 @@ class SiamMultiTracker(object):
             self.states.append(state)
 
         # make result
-        result = np.zeros((len(self.states) + len(not_tracked_boxes), not_tracked_boxes.shape[1] + 2), dtype=np.float32) - 1.
+        result = np.zeros((len(self.states) + len(not_tracked_boxes), not_tracked_boxes.shape[1] + 2),
+                          dtype=np.float32) - 1.
         for i, s in enumerate(self.states):
             result[i][:4] = s['box']
             ibox = s['input_box']
@@ -115,6 +114,6 @@ class SiamMultiTracker(object):
 
         return result
 
-    def remove_tracks(self,track_ids):
+    def remove_tracks(self, track_ids):
         track_ids = set([int(i) for i in track_ids])
-        self.states = [s for s in self.states if not( int(s['track_id']) in track_ids)]
+        self.states = [s for s in self.states if not (int(s['track_id']) in track_ids)]
