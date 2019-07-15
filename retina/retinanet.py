aa2db94582bcccd3811ff57f84ac05163f19334a
Lionel Miller <delonorm@gmail.com>
2019-07-15 13:10:13 +0300

    Hotfix for imports caused by sys.path carnage

--- retina/retinanet.py
+++ retina/retinanet.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from resnet import *
+from retina_resnet import *
 from efnet import EfficientNet
 from itertools import chain
 
